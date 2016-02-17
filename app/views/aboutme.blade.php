8ce018348326e74f3dd9305fa9c84144c5166c26
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-02-16 20:45:27 -0600

    edited the aboutme

--- app/views/aboutme.blade.php
+++ app/views/aboutme.blade.php
@@ -4,7 +4,7 @@
 		<div class="col-lg-12">
 			<div class="textsection text-center">
 				<h3>
-					My name is Bo Wilkins, and I am many things. First and foremost, I am a pet, but I am also the first Black Labrador to run for the position of President of the United States. I am a native Texan born in Houston in 2007 to an unknown owner. At the age of two years, I was hit by a car near Katy, and I was taken to an animal rescue facility. Many months later, O found myself in the care of the Wilkins family. Since that day, I have been a dedicated advocate of food and eating, as well as being petted. I hope to share this joy I have found with the rest of the country as President. Allow me to give you some of the reasons I am qualified for this office.
+					My name is Bo Wilkins, and I am many things. First and foremost, I am a pet, but I am also the first Black Labrador to run for the position of President of the United States. I am a native Texan born in Houston in 2007 to an unknown owner. At the age of two years, I was hit by a car near Katy, and I was taken to an animal rescue facility. Many months later, O found myself in the care of the Wilkins family. Since that day, I have been a dedicated advocate of food and eating, as well as being petted. I hope to share this joy I have found with the rest of the country as President. Allow me to give you some of the reasons I am qualified for the office of the presidency.
 				</h3>
 				<h4>
 					My Awards:

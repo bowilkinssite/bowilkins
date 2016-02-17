de111d5dcdf0c734f894ab810343e2cf33a61c24
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-02-17 13:08:45 -0600

    added some more bootstrap to the about page to test some new mobile-friendly measures

--- app/views/aboutme.blade.php
+++ app/views/aboutme.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
 	<div class="row">
-		<div class="col-lg-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			<div class="textsection text-center">
 				<h3>
 					My name is Bo Wilkins, and I am many things. First and foremost, I am a pet, but I am also the first Black Labrador to run for the position of President of the United States. I am a native Texan born in Houston in 2007 to an unknown owner. At the age of two years, I was hit by a car near Katy, and I was taken to an animal rescue facility. Many months later, I found myself in the care of the Wilkins family. Since that day, I have been a dedicated advocate of food and eating, as well as being petted. I hope to share this joy I have found with the rest of the country as President. Allow me to give you some of the reasons I am qualified for this office.

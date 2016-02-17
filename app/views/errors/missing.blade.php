73f99d73b84ba270fbb98730e22d68ef56d60ca3
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-02-17 03:30:22 -0600

    added a new 404 page

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('content')
+	<div class="row text-center">
+		<div class="col-lg-12">
+			<div class="textsection">
+				<h1>This page is not found!</h1>
+			</div>
+		</div>
+	</div>
+@stop

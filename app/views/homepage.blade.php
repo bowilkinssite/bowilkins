bf1d090dbcae93c7b522054ea76bb731694b52ab
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-02-19 16:06:43 -0600

    edited the landing page, tried out a new, more realistic style

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -1,55 +1,29 @@
-@extends('layouts.master')
-@section('content')
-	<div class="row textsection">
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bo Wilkins for President</title>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/css/bowilkins.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row landing1">
 		<div class="col-lg-12">
-			<div class="homepagegreeter text-center">
-				<h1>Welcome to the official Bo Wilkins for President webpage!</h4>
+					<span class="col-lg-6 col-lg-offset-2 col-md-6 col-md-offset-2 col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-1 landingquote"><img class="smallmotto" src="/img/smallmotto.png"></span>
+			<div class="landingback">
 			</div>
+			{{-- <div class="divider1"></div>
+			<div class="divider2"></div> --}}
 		</div>
 	</div>
-	<div class="row">
+	<div class="row landing2 text-center">
 		<div class="col-lg-12">
-			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-			  <!-- Indicators -->
-			  <ol class="carousel-indicators">
-			    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-			  </ol>
-
-			  <!-- Wrapper for slides -->
-			  <div class="carousel-inner" role="listbox">
-			    <div class="item active">
-			      <img src="/img/bo1.png">
-			      <div class="carousel-caption">
-			        Just picture this face leading the country
-			      </div>
-			    </div>
-			    <div class="item">
-			      <img src="/img/bo2.png">
-			      <div class="carousel-caption">
-			        I carry a big stick!
-			      </div>
-			    </div>
-			    <div class="item">
-			    	<img src="/img/bo3.png">
-			    	<div class="carousel-caption">
-			    		Never resting until the job is done...or until after dinnertime.
-			    	</div>
-			    </div>
-			    ...
-			  </div>
-
-			  <!-- Controls -->
-			  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-			    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			    <span class="sr-only">Previous</span>
-			  </a>
-			  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-			    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			    <span class="sr-only">Next</span>
-			  </a>
+			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+				Contact Bo
 			</div>
+			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">Continue to Site</div>
 		</div>
 	</div>
-@stop
+</div>
+</body>
+</html>

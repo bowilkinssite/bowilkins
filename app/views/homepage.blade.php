0d54df590b56acd0dd200e6235eefb134fcc2dd4
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-02-19 16:24:03 -0600

    I believe that the height attribute is not actually having any impact. Testing this theory on larger and smaller screens

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -9,7 +9,7 @@
 <div class="container-fluid">
 	<div class="row landing1">
 		<div class="col-lg-12">
-					<span class="col-lg-6 col-lg-offset-2 col-md-6 col-md-offset-2 col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-1 landingquote"><img class="smallmotto" src="/img/smallmotto.png"></span>
+					<span class="col-lg-6 col-lg-offset-2 col-md-6 col-md-offset-2 col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-2 landingquote"><img class="smallmotto" src="/img/smallmotto.png"></span>
 			<div class="landingback">
 			</div>
 			{{-- <div class="divider1"></div>

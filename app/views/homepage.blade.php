537371820de2d27bfeae96b5248fb1907f2c300f
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-02-22 20:29:34 -0600

    Added in a specific row for sm and xs

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -23,11 +23,13 @@
 		</div>
 	</div>
 	<div id="thefooter" class="row hidden-sm hidden-xs landing2 text-center">
-		<div class="col-lg-12">
-			<a href="{{{action('HomeController@contact')}}}"><div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-				Contact Bo
-			</div></a>
-			<a href="{{{action('HomeController@aboutme')}}}"><div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">Continue to Site</div></a>
+		<div class="row hidden-lg hidden-md smalllanding2">
+			<div class="col-lg-12">
+				<a href="{{{action('HomeController@contact')}}}"><div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+					Contact Bo
+				</div></a>
+				<a href="{{{action('HomeController@aboutme')}}}"><div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">Continue to Site</div></a>
+			</div>
 		</div>
 	</div>
 </div>

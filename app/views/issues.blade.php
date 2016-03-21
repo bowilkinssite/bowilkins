90c5c65212b37c0d412490882cd5d424ab6bbc6e
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-03-21 16:32:14 -0500

    Edited some of the issues entries

--- app/views/issues.blade.php
+++ app/views/issues.blade.php
@@ -10,15 +10,15 @@
 				</span>
 				<h4 class="issuesheading">Church and State</h4>
 				<span class="issuestext">
-					I am not in favor of the separation of church and Steak. Why is it that the government believes it has the right to tell me whether I can or cannot eat steak in church? Isn't that up to the church involved?
+					I am not in favor of the separation of church and steak. Why is it that the government believes it has the right to tell me whether I can or cannot eat steak in church? Isn't that up to the church involved?
 				</span>
 				<h4 class="issuesheading">Foreign Relations</h4>
 				<span class="issuestext">
-					I am especially proud of my experience in this area! I have travelled to many foreign countries, including: the garage, the doctor's office, the pet salon, and the neighbor man's backyard! As president, I'll be realy nice to al the people at those places so that theyd on't bite us and they give us pettins. I also undersand that Iran has a "new clear deal." When I figure out what this "new clear deal" is, I'll let you know how I can get it to pet us!
+					I am especially proud of my experience in this area! I have travelled to many foreign countries, including: the garage, the doctor's office, the pet salon, and the neighbor man's backyard! As president, I'll be realy nice to all the people at those places so that they won't bite us and they give us pettins. I also undersand that Iran has a "new clear deal." When I figure out what this "new clear deal" is, I'll let you know how I can get it to pet us!
 				</span>
 				<h4 class="issuesheading">Response to Foreign Military Aggression</h4>
 				<span class="issuestext">
-					In my many years in the Backyard, many threats have reared their heads. They have threatened my life, the lives of my siblings, and indeed the lives of my owners. These threats include: squirrels, birds, little frogs, big frogs, tutles, the dogs that live next door, the old basset hound that sometimes escapes, kitty cats, the garbage truck, running women, and many other dogs who have dared to approach our borders. I have found that the best solution for dealing with foreign military aggression is to bark at it for a little while, and eventually it will go away. As president, I will implement this policy in the face of all foreign aggression.
+					In my many years in the Backyard, many threats have reared their heads. They have threatened my life, the lives of my siblings, and indeed the lives of my owners. These threats include: squirrels, birds, little frogs, big frogs, turtles, the dogs that live next door, the old basset hound that sometimes escapes, kitty cats, the garbage truck, running women, and many other dogs who have dared to approach our borders. I have found that the best solution for dealing with foreign military aggression is to bark at it for a little while, and eventually it will go away. As president, I will implement this policy in the face of all foreign aggression.
 				</span>
 				<h4 class="issuesheading">Education</h4>
 				<span class="issuestext">
@@ -26,15 +26,19 @@
 				</span>
 				<h4 class="issuesheading">Taxation</h4>
 				<span class="issuestext">
-					Whenever I try to eat some of Spare Dog's food, I get yelled at, and they tell me to eat my own food. But I'm still hungry after I eat my own food, and Spare Dog doesn't even eat it. As president, I would passs legislation allowing people who are still hungry to eat someone else's food if they don't want it.
+					Whenever I try to eat some of Spare Dog's food, I get yelled at, and they tell me to eat my own food. But I'm still hungry after I eat my own food, and Spare Dog doesn't even eat it. As president, I would pass legislation allowing people who are still hungry to eat someone else's food if they don't want it.
 				</span>
 				<h4 class="issuesheading">Gun Control</h4>
 				<span class="issuestext">
-					I'm a hunting dog. Connect the dots. 
+					As a Labrador Retriever, hunting is in my blood. If people don't have access to firearms, then how are owners going to get dog food for their dogs? And what happens if a bird or a frog tries to invade the yard when the dogs are in the garage? Guns are really loud and really scary, but it should be up to the individual to choose whether they hunt for their large breed dog food themselves or pay a hunter to hunt it for them.  
 				</span>
 				<h4 class="issuesheading">Healthcare</h4>
 				<span class="issuestext">
 					I don't like going to the vet. Sometimes, my owners get me in the car and I think they're going to take me for a walk, but then we go to the vet. As president, I will make sure that no American ever has to go to the vet!
+				</span>
+				<h4 class="issuesheading">Immigration</h4>
+				<span class="issuestext">
+					I am in favor of greatly increasing our border security. As I mentioned in my stance on response to foreign military aggression, the threats that face our country are too many to name. As it stands, it is all too easy for birds, frogs, snakes, and little bugs that bite to get through the fence. As president, one of my top priorities will be to make it impossible for anything to get into the backyard without passing a thorough background check to make sure they aren't here to steal our food or bite us. 
 				</span>
 			</div>
 		</div>
